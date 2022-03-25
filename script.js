f150b0dbae650b30aaf45b7bcd512af0f216715f
Pitri Indrayani <indrayanipitri@gmail.com>
2022-03-25 15:20:37 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+    // let select = document.getElementById("select");
+
+    // select.onmouseover=function(){
+    //     select.style.backgroundColor = "red"
+    // }
+
+    // select.onmouseout=function(){
+    //     select.style.backgroundColor = "transparent"
+    // }
+
+function selectImg() {
+    let select = document.getElementsByClassName("select-img");
+
+    select[0].style.backgroundColor = "#C4C4C4";
+    select[0].style.height = "150px";
+    select[0].style.width = "150px";
+    select[0].style.margin = "auto";
+    select[0].style.marginTop = "30px";
+    select[0].style.borderRadius = "15%"
+}
