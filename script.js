52f6bdd0795949c9589a0dc664db34e601afb890
Pitri Indrayani <indrayanipitri@gmail.com>
2022-03-28 23:49:34 +0700

    28/03/22

--- script.js
+++ script.js
@@ -1,21 +1,71 @@
 
-    // let select = document.getElementById("select");
+    const ROCK = 0;
+    const PAPER = 1;
+    const SCUISSOR = 2;
 
-    // select.onmouseover=function(){
-    //     select.style.backgroundColor = "red"
-    // }
+    function selectImg(indexElement) {
+        let select = document.getElementsByClassName("select-img");
 
-    // select.onmouseout=function(){
-    //     select.style.backgroundColor = "transparent"
-    // }
+        select[indexElement].style.backgroundColor = "#C4C4C4";
+        select[indexElement].style.height = "150px";
+        select[indexElement].style.width = "150px";
+        select[indexElement].style.margin = "auto";
+        select[indexElement].style.marginTop = "30px";
+        select[indexElement].style.borderRadius = "15%"
+    }
 
-function selectImg() {
-    let select = document.getElementsByClassName("select-img");
+    function getComputer() {
+        const comp = Math.random();
+        if(comp < 0.34) return 'batu';
+        if(comp >= 0.34 && comp < 0.67) return 'gunting';
+        return 'kertas';
+    }
 
-    select[0].style.backgroundColor = "#C4C4C4";
-    select[0].style.height = "150px";
-    select[0].style.width = "150px";
-    select[0].style.margin = "auto";
-    select[0].style.marginTop = "30px";
-    select[0].style.borderRadius = "15%"
-}
+    function getResult(comp, player) {
+        if (player == comp) return "SERI";
+        if (player == 'batu') return (comp == 'kertas') ? "PLAYER KALAH" : "COMPUTER MENANG";
+        if (player == 'gunting') return (comp == 'batu') ? 'PLAYER MENANG' : ' COMPUTER MENANG';
+        if (player == 'kertas') return (comp == 'gunting') ? 'PLAYER KALAH' : ' COMPUTER MENANG';
+    } 
+
+    const pilihBatu = document.querySelector('.batu');
+    pilihBatu.addEventListener('click', function() {
+        const computerPilih = getComputer();
+        const playerPilih = pilihBatu.className;
+        const hasil = getResult(computerPilih, playerPilih);
+
+        let imgComp = document.querySelector('.img-comp');
+        imgComp.setAttribute('src', 'assets/' + computerPilih + '.png')
+
+       const info = document.querySelector('.info');
+       info.innerHTML = hasil;
+    });
+
+    const pilihGunting = document.querySelector('.gunting');
+    pilihGunting.addEventListener('click', function() {
+        const computerPilih = getComputer();
+        const playerPilih = pilihGunting.className;
+        const hasil = getResult(computerPilih, playerPilih);
+      
+
+        let imgComp = document.querySelector('.img-comp');
+        imgComp.setAttribute('src', 'assets/' + computerPilih + '.png')
+
+       const info = document.querySelector('.info');
+       info.innerHTML = hasil;
+    });
+
+    const pilihKertas = document.querySelector('.kertas');
+    pilihKertas.addEventListener('click', function() {
+        const computerPilih = getComputer();
+        const playerPilih = pilihKertas.className;
+        const hasil = getResult(computerPilih, playerPilih);
+      
+
+        let imgComp = document.querySelector('.img-comp');
+        imgComp.setAttribute('src', 'assets/' + computerPilih + '.png')
+
+       const info = document.querySelector('.info');
+       info.innerHTML = hasil;
+    });
+
